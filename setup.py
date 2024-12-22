689fdfe7331f7284259d2d4581e1aefa8af1e43c
Paul Gierz <pgierz@mac.com>
2024-12-21 16:06:20 -0800

    style(setup.py): apply black style

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup
 
 setup(
-    name='gpg-subkey-generator',
-    version='0.1.0',
-    py_modules=['gpg_subkey_generator'],
-    install_requires=[
-        'python-gnupg',
-        'questionary'
-    ],
-    entry_points='''
+    name="gpg-subkey-generator",
+    version="0.1.0",
+    py_modules=["gpg_subkey_generator"],
+    install_requires=["python-gnupg", "questionary"],
+    entry_points="""
         [console_scripts]
         dots-tools=cli:main
         gpg-subkey-generator=gpg_subkey_generator:main
-    ''',
-    author='Paul Gierz',
-    author_email='pgierz@mac.com',
-    description='A tool to generate GPG subkeys interactively',
-    keywords='GPG subkeys encryption signing authentication',
+    """,
+    author="Paul Gierz",
+    author_email="pgierz@mac.com",
+    description="A tool to generate GPG subkeys interactively",
+    keywords="GPG subkeys encryption signing authentication",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
-
